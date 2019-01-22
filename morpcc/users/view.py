c2508da9112654d82a144e4552457ec5d1cce3b5
Izhar Firdaus <kagesenshi.87@gmail.com>
2019-01-22 17:15:42 +0800

    comment out user view for now

--- morpcc/users/view.py
+++ morpcc/users/view.py
@@ -3,6 +3,7 @@ from .model import UserModelUI
 from morpfw.crud import permission as crudperms
 
 
-@App.html(model=UserModelUI, permission=crudperms.View, template='auth/user.pt')
-def view(context, request):
-    return {}
+# @App.html(model=UserModelUI, name='view', permission=crudperms.View, template='auth/user.pt')
+# def view(context, request):
+#	return {}
+#
