6fe2842239b669aa2c5d952f3d547bf4ba575265
Izhar Firdaus <kagesenshi.87@gmail.com>
2019-02-13 17:29:15 +0800

    reorganize static directory location

--- morpcc/static.py
+++ morpcc/static.py
@@ -1,5 +1,6 @@
 from .app import App
 from webob import static
+from webob.exc import HTTPNotFound, HTTPUnauthorized
 from pkg_resources import resource_filename
 import os
 
@@ -10,21 +11,31 @@ class StaticRoot(object):
     directory = 'static_files'
 
     def __init__(self, path):
-        self.path = path
+        self.path = path or ''
 
     def resource_path(self):
+        if not self.path.strip():
+            return None
         d = resource_filename(self.module, self.directory)
         return os.path.join(d, self.path)
 
 
-@App.path(model=StaticRoot, path='/static', absorb=True)
+@App.path(model=StaticRoot, path='/__static__/morpcc', absorb=True)
 def get_staticroot(absorb):
     return StaticRoot(absorb)
 
 
 @App.view(model=StaticRoot)
 def serve_static(context, request):
-    return request.get_response(static.FileApp(context.resource_path()))
+    path = context.resource_path()
+    if not path:
+        raise HTTPNotFound()
+    resp = request.get_response(static.FileApp(path))
+    if resp.status_code == 404:
+        raise HTTPNotFound()
+    if resp.status_code == 403:
+        raise HTTPUnauthorized()
+    return resp
 
 
 class DeformStaticRoot(StaticRoot):
@@ -33,6 +44,6 @@ class DeformStaticRoot(StaticRoot):
     directory = 'static'
 
 
-@App.path(model=DeformStaticRoot, path='/deform_static', absorb=True)
+@App.path(model=DeformStaticRoot, path='/__static__/deform', absorb=True)
 def get_deformstaticroot(absorb):
     return DeformStaticRoot(absorb)
