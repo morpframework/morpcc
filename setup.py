70331e0d0127ddb26b22266bc144514f12a99389
Izhar Firdaus <kagesenshi.87@gmail.com>
2019-02-14 17:30:30 +0800

    bump to morpfw 0.2.1rc4

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ A meta CMS built on top of MorpFW""",
       zip_safe=False,
       install_requires=[
           # -*- Extra requirements: -*-
-          "morpfw==0.2.1rc2",
+          "morpfw==0.2.1rc4",
           "more.chameleon",
           "more.static",
           "deform",
