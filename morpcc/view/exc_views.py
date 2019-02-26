1f7d22154404ebac9a1c8861d7e7e9678297bd57
Izhar Firdaus <kagesenshi.87@gmail.com>
2019-02-26 10:39:59 +0800

    display different error output for /api/ and normal pages

--- morpcc/view/exc_views.py
+++ morpcc/view/exc_views.py
@@ -7,18 +7,26 @@ from ..app import App
 from ..root import Root
 
 
-@App.html(model=HTTPNotFound, template='master/error_404.pt')
-def httpnotfound_error(context, request):
+@App.view(model=HTTPNotFound)
+def httpnotfound_error(context, request: morepath.Request):
+
     @request.after
     def adjust_status(response):
         response.status = 404
-    return {'status': 'error',
-            'message': 'Object Not Found : %s' % request.path}
+
+    if request.path.startswith('/api/'):
+        return morepath.render_json({
+            'status': 'error',
+            'message': 'Object Not Found : %s' % request.path}, request)
+    else:
+        render = request.app.config.template_engine_registry.get_template_render(
+            'master/error_404.pt', morepath.render_html)
+        return render({}, request)
 
 
-@App.html(model=HTTPForbidden, template="master/error_403.pt")
+@App.view(model=HTTPForbidden)
 def forbidden_error(context, request):
-    if request.identity is NO_IDENTITY:
+    if request.identity is NO_IDENTITY and not request.path.startswith('/api/'):
         return morepath.redirect('/login?came_from=%s' % urllib.parse.quote(request.url))
 
     @request.after
@@ -26,5 +34,12 @@ def forbidden_error(context, request):
         response.status = 403
 #   FIXME: should log this when a config for DEBUG_SECURITY is enabled
 #    logger.error(traceback.format_exc())
-    return {'status': 'error',
-            'message': 'Access Denied : %s' % request.path}
+    if request.path.startswith('/api/'):
+        return morepath.render_json({
+            'status': 'error',
+            'message': 'Access Denied : %s' % request.path}, request)
+    else:
+        render = request.app.config.template_engine_registry.get_template_render(
+            'master/error_403.pt', morepath.render_html
+        )
+        return render({}, request)
