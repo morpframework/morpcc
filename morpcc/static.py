130a7e1f521063e39e446192cd11e796d8648ab4
Izhar Firdaus <kagesenshi.87@gmail.com>
2019-05-10 11:12:09 +0800

    add caching headers for static files

--- morpcc/static.py
+++ morpcc/static.py
@@ -1,8 +1,14 @@
 from .app import App
 from webob import static
-from webob.exc import HTTPNotFound, HTTPUnauthorized
+from webob.exc import HTTPNotFound, HTTPUnauthorized, HTTPNotModified
 from pkg_resources import resource_filename
 import os
+import hashlib
+from datetime import datetime, timedelta
+import morepath
+
+ETAG = hashlib.md5(datetime.now().strftime(
+    r'%Y%d%d%H').encode('ascii')).hexdigest()
 
 
 class StaticRoot(object):
@@ -30,11 +36,36 @@ def serve_static(context, request):
     path = context.resource_path()
     if not path:
         raise HTTPNotFound()
+
+    settings = request.app.settings.__dict__
+    max_age = int(settings.get(
+        'caching', {}).get('max-age', 30))
+
+    etag = request.headers.get('If-None-Match', '')
+
+    def add_headers(response):
+        response.headers.add('Cache-Control', 'max-age=%s' % max_age)
+        response.headers.add('Expires', (
+            datetime.utcnow() +
+            timedelta(seconds=max_age)).strftime(r'%a, %d %b %Y %H:%M:%S GMT'))
+        response.headers.add('ETag', ETAG)
+
+    if etag and etag == ETAG:
+        @request.after
+        def add_notmodified_headers(response):
+            add_headers(response)
+
+        return morepath.Response(status=304)
+
     resp = request.get_response(static.FileApp(path))
     if resp.status_code == 404:
         raise HTTPNotFound()
     if resp.status_code == 403:
         raise HTTPUnauthorized()
+
+    @request.after
+    def add_caching_headers(response):
+        add_headers(response)
     return resp
 
 
