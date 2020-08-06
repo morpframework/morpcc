be3a1c4f9af9c8d13bea46c0249d268665539222
Izhar Firdaus <kagesenshi.87@gmail.com>
2020-08-06 13:22:55 +0800

    allow cancelling / failing from new

--- morpcc/process/statemachine.py
+++ morpcc/process/statemachine.py
@@ -1,7 +1,8 @@
 from datetime import datetime
 
-import morpfw
 import pytz
+
+import morpfw
 
 from ..app import App
 from .model import ProcessModel
@@ -12,8 +13,8 @@ class ProcessStateMachine(morpfw.StateMachine):
     states = ["new", "running", "cancelled", "failed", "success"]
     transitions = [
         {"trigger": "start", "source": "new", "dest": "running"},
-        {"trigger": "cancel", "source": "running", "dest": "cancelled"},
-        {"trigger": "fail", "source": "running", "dest": "failed"},
+        {"trigger": "cancel", "source": ["running", "new"], "dest": "cancelled"},
+        {"trigger": "fail", "source": ["running", "new"], "dest": "failed"},
         {"trigger": "complete", "source": "running", "dest": "success"},
     ]
 
