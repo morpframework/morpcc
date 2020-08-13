23870c02f792a6f27d2d784431b0e21d7e5447f2
Izhar Firdaus <kagesenshi.87@gmail.com>
2020-08-13 15:15:00 +0800

    make application uuid searchable

--- morpcc/process/schema.py
+++ morpcc/process/schema.py
@@ -11,7 +11,7 @@ from ..deform.codewidget import CodeWidget, JSONCodeWidget
 class ProcessSchema(morpfw.Schema):
 
     signal: typing.Optional[str] = field(
-        default=None, metadata={"required": True, "editable": False}
+        default=None, metadata={"required": True, "editable": False, "searchable": True}
     )
     task_id: typing.Optional[str] = field(
         default=None, metadata={"format": "uuid", "required": True, "editable": False}
@@ -19,7 +19,8 @@ class ProcessSchema(morpfw.Schema):
     start: typing.Optional[datetime] = field(default=None, metadata={"editable": False})
     end: typing.Optional[datetime] = field(default=None, metadata={"editable": False})
     params: typing.Optional[dict] = field(
-        default_factory=dict, metadata={"editable": False, "deform.widget": JSONCodeWidget()}
+        default_factory=dict,
+        metadata={"editable": False, "deform.widget": JSONCodeWidget()},
     )
     traceback: typing.Optional[str] = field(
         default=None,
