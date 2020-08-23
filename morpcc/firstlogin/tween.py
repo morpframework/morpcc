b559ed5d22ab0eef4bcb6bda24fa7be69485638f
Izhar Firdaus <kagesenshi.87@gmail.com>
2020-08-24 01:42:50 +0800

    made user activation screen optional

--- morpcc/firstlogin/tween.py
+++ morpcc/firstlogin/tween.py
@@ -1,6 +1,7 @@
-from ..app import App
-from morpfw.authn.pas.path import get_user_collection
 import morepath
+from morpfw.authn.pas.path import get_user_collection
+
+from ..app import App
 
 EXCLUDE_PREFIXES = ["/__static__/"]
 EXCLUDE_PATHS = ["/logout", "/send-verification", "/verify"]
@@ -8,6 +9,10 @@ EXCLUDE_PATHS = ["/logout", "/send-verification", "/verify"]
 
 @App.tween_factory()
 def make_tween(app, handler):
+    verify_email = app.get_config("morpcc.registration_verify_email", False)
+    if not verify_email:
+        return handler
+
     def redirect_to_firstlogin(request: morepath.Request):
         for path in EXCLUDE_PREFIXES:
             if request.path.startswith(path):
