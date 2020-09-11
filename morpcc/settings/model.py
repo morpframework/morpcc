164356332573eab18628491fbbddccbaa2444ccd
Izhar Firdaus <kagesenshi.87@gmail.com>
2020-09-11 16:01:00 +0800

    added convenience API for resolving setting key

--- morpcc/settings/model.py
+++ morpcc/settings/model.py
@@ -4,6 +4,8 @@ import rulez
 from .modelui import SettingCollectionUI, SettingModelUI
 from .schema import SettingSchema
 
+_marker = object()
+
 
 class SettingModel(morpfw.Model):
     schema = SettingSchema
@@ -23,3 +25,31 @@ class SettingCollection(morpfw.Collection):
         if items:
             return items[0]
         return self.create({"key": key, "value": None})
+
+    def resolve(self, key, default=_marker):
+        item = self.get_by_key(key)
+        pages = self.request.app.config.setting_page_registry.values(self.request)
+        for p in pages:
+            schema = p.factory(self.request)
+            for fname, field in schema.__dataclass_fields__.items():
+                if field.metadata.get("morpcc.setting.key", None) == key:
+                    fschema = p.jsonformschema(self, self.request)
+                    serde = fschema[fname]
+                    return serde.deserialize(item["data"]["value"])
+        if default is _marker:
+            raise KeyError(key)
+        return default
+
+    def store(self, key, value):
+        item = self.get_by_key(key)
+        pages = self.request.app.config.setting_page_registry.values(self.request)
+        for p in pages:
+            schema = p.factory(self.request)
+            for fname, field in schema.__dataclass_fields__.items():
+                if field.metadata.get("morpcc.setting.key", None) == key:
+                    fschema = p.jsonformschema(self, self.request)
+                    serde = fschema[fname]
+                    value = serde.serialize(value)
+                    item.update({"data": {"value": value}})
+                    return
+        raise KeyError(key)
