2b92c8c5ba1f89dbd65f6c812186e640e5e3ac6e
Izhar Firdaus <kagesenshi.87@gmail.com>
2020-10-26 23:10:55 +0800

    made entitypermassignment internal

--- morpcc/entitypermissionassignment/typeinfo.py
+++ morpcc/entitypermissionassignment/typeinfo.py
@@ -1,28 +1,32 @@
 from ..app import App
-from .model import EntityPermissionAssignmentCollection
-from .model import EntityPermissionAssignmentModel
-from .schema import EntityPermissionAssignmentSchema
+from .model import EntityPermissionAssignmentCollection, EntityPermissionAssignmentModel
+
+#
+from .modelui import (
+    EntityPermissionAssignmentCollectionUI,
+    EntityPermissionAssignmentModelUI,
+)
 from .path import get_collection, get_model
-# 
-from .modelui import EntityPermissionAssignmentCollectionUI
-from .modelui import EntityPermissionAssignmentModelUI
-# 
+from .schema import EntityPermissionAssignmentSchema
+
+#
 
 
 @App.typeinfo(
-    name='morpcc.entitypermissionassignment',
-    schema=EntityPermissionAssignmentSchema)
+    name="morpcc.entitypermissionassignment", schema=EntityPermissionAssignmentSchema
+)
 def get_typeinfo(request):
     return {
-        'title': 'EntityPermissionAssignment',
-        'description': 'EntityPermissionAssignment type',
-        'schema': EntityPermissionAssignmentSchema,
-        'collection': EntityPermissionAssignmentCollection,
-        'collection_factory': get_collection,
-        'model': EntityPermissionAssignmentModel,
-        'model_factory': get_model,
-        # 
-        'collection_ui': EntityPermissionAssignmentCollectionUI,
-        'model_ui': EntityPermissionAssignmentModelUI,
-        # 
+        "title": "EntityPermissionAssignment",
+        "description": "EntityPermissionAssignment type",
+        "schema": EntityPermissionAssignmentSchema,
+        "collection": EntityPermissionAssignmentCollection,
+        "collection_factory": get_collection,
+        "model": EntityPermissionAssignmentModel,
+        "model_factory": get_model,
+        #
+        "collection_ui": EntityPermissionAssignmentCollectionUI,
+        "model_ui": EntityPermissionAssignmentModelUI,
+        "internal": True
+        #
     }
