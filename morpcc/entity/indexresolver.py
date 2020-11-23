3a4ac03b4dea94dd56878028e1b334242c5306cb
Izhar Firdaus <kagesenshi.87@gmail.com>
2020-11-23 10:55:07 +0800

    fix site-search indexing

--- morpcc/entity/indexresolver.py
+++ morpcc/entity/indexresolver.py
@@ -7,6 +7,10 @@ from .path import get_model as get_entity
 @App.indexresolver("morpcc.entity.content")
 def resolve(context, request):
     app = get_application(request, context["application_uuid"])
+    if not app:
+        return
     dm = get_entity(request, context["entity_uuid"])
+    if not dm:
+        return
     col = content_collection_factory(dm, app)
     return col.get(context["entity_content_uuid"])
